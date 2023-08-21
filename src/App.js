5e95575596eec8883c034d4cb7d6a1ce6c67d4f7
Zdravko Danailov <zdravkodanailov7@gmail.com>
2023-08-21 09:25:09 +0100

    create header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { Header, Tasks } from './components'
 
 function App() {
+  const [tasks, setTasks] = useState([
+  {
+    id: 1,
+    text: 'Doctors Appointment',
+    day: 'Tuesday',
+    reminder: true
+  }
+])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+          <Header />
+          <Tasks tasks={tasks} />
+      </div>
+    </>
   );
 }
 
