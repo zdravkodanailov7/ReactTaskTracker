c2f80fde1ad2026bdbf5ae0c9df3456ab5bce33d
Zdravko Danailov <zdravkodanailov7@gmail.com>
2023-08-28 23:28:01 +0100

    Style Upgrades

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,8 @@
 export { default as Header } from './Header/Header';
 export { default as Upgrade } from './UpgradeCTA/Upgrade';
 export { default as Button } from './Button/Button';
-export { default as Tasks } from './Tasks';
-export { default as Task } from './Task';
+export { default as Tasks } from './Tasks/Tasks';
+export { default as Task } from './Tasks/Task/Task';
 export { default as AddTask } from './AddTask';
 export { default as Footer } from './Footer';
 export { default as About } from './About';
